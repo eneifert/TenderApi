e295b438f5d9578550f4171052fb54ee84977201
Eric Neifert <eric.neifert@gmail.com>
2010-12-23 14:08:02 -0500

    Added dummy settings

---
+++ TenderApiTests/TenderSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenderApiTests
+{
+    public class TenderSettings
+    {
+        public const string Site = "SITE";
+        public const string Email = "EMAIL";
+        public const string UserName = "USER";
+        public const string Password = "PASSWORD";
+        public const string ApiKey = "API_KEY";
+    }
+}
